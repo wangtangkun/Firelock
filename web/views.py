7621960cb54168355cf313791597f4f712ab714b
wangtangkun <496996740@qq.com>
2020-07-24 18:36:11 +0800

    login

--- web/views.py
+++ web/views.py
@@ -12,9 +12,12 @@ def login(request):
     if request.method=="POST":
         name=request.POST.get("name")
         password=request.POST.get("password")
-        baoliu=request.POST.get("baoliu")
-        print("name",name,"password",password,"Keep_account",Keep_account)
+        Keep_account=request.POST.get("Keep_account")
+        email=request.POST.get("email")
+        print(email)
+        print("name",name,"password",password,"Keep_account",Keep_account,email)
     error_msg = ""
     if request.POST.get("name")=="123":error_msg = "用户名密码错误!"
     #2123
     return render(request, "login/login.html", {"error_msg": error_msg})
+
